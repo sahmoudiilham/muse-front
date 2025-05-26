5759f513870efacb6cd4c798fd18b5bcb6ac1bcb
sahmoudi ilham <sahmoudiilham4@gmail.com>
2025-05-27 00:35:05 +0100

    Ajout de la page Vetement avec son style SCSS

---
+++ src/pages/Vetement.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/vetement.scss";
+
+const Vetement = () => {
+  return (
+    <div className="vetement-page">
+      <h1>Conseils de vêtements</h1>
+      <p>Choisissez les vêtements adaptés à votre morphologie et votre style.</p>
+      {/* يمكنكِ لاحقًا إضافة صور أو فلاتر حسب نوع الجسم أو المناسبات */}
+    </div>
+  );
+};
+
+export default Vetement;
