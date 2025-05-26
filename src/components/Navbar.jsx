d831590137c772baff8474272dd1c44e7f8c10c6
sahmoudi ilham <sahmoudiilham4@gmail.com>
2025-05-26 23:18:19 +0100

    add: Footer component with styles

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">Muse</Link>
+      </div>
+      <ul className="nav-links">
+        <li><Link to="/morphologie">Morphologie</Link></li>
+        <li><Link to="/palette">Palette</Link></li>
+        <li><Link to="/vetement">Vêtements</Link></li>
+        <li><Link to="/coiffure">Coiffure</Link></li>
+        <li><Link to="/lunette">Lunettes</Link></li>
+        <li><Link to="/visage">Visage</Link></li>
+        <li><Link to="/favoris">Favoris</Link></li>
+        <li><Link to="/chat">Chat IA</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
