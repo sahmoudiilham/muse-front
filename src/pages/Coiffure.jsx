2bdb41dbd888c2299fb19a9ecf7f864bb75b1ff0
sahmoudi ilham <sahmoudiilham4@gmail.com>
2025-05-27 00:38:02 +0100

    Add Coiffure page with SCSS styling

---
+++ src/pages/Coiffure.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/coiffure.scss';
+
+const Coiffure = () => {
+  return (
+    <div className="coiffure-page">
+      <h2>Conseils de Coiffure</h2>
+      <p>
+        Découvrez quelles coiffures s’adaptent le mieux à la forme de votre visage et à votre style personnel.
+      </p>
+      {/* Tu peux ajouter des exemples d'images ou un slider ici plus tard */}
+    </div>
+  );
+};
+
+export default Coiffure;
