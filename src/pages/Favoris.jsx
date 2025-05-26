ddc1b0cadf3cb87db7e7b40a22c150c3cc19efc2
sahmoudi ilham <sahmoudiilham4@gmail.com>
2025-05-27 00:36:15 +0100

    Add Favoris page and styling

---
+++ src/pages/Favoris.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/favoris.scss";
+
+const Favoris = () => {
+  return (
+    <div className="favoris-page">
+      <h2>Mes favoris</h2>
+      <p>Ici, vous trouverez tous vos articles préférés.</p>
+    </div>
+  );
+};
+
+export default Favoris;
